db5a28f9f9083b1fe83adc3922cdd8d2b0dfe5f8
abelT19 <abeltariku43@gmail.com>
2025-10-19 04:19:21 -0700

    Modify customer insertion and rename Customers table
    Updated the SQL script to insert a customer and rename the Customers table to customer.

--- task_5.sql
+++ task_5.sql
@@ -1,26 +1,4 @@
 -- task_5.sql
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
-import mysql.connector
-
-try:
-    conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="1221SmTa",
-        database="alx_book_store"
-    )
-    cursor = conn.cursor()
-    cursor.execute("""
-    INSERT INTO Customers (customer_id, customer_name, email, address)
-    VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
-    """)
-    conn.commit()
-    print("Customer inserted successfully!")
-except mysql.connector.Error as err:
-    print(f"Error: {err}")
-finally:
-    if conn.is_connected():
-        cursor.close()
-        conn.close()
-
+RENAME TABLE Customers TO customer;
